d3b98336e946ab25c2eca668168d17d1e20d31d5
Vimal Mathiyazhagan <iamvimal28@gmail.com>
2021-05-14 02:29:26 +0530

    adding features

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
+
+const Header = props => {
+    return <React.Fragment>
+        <header className={classes.header}>
+            <h1>Hot Meals</h1>
+            <HeaderCartButton />
+        </header>
+        <div className={classes['main-image']}>
+            <img src={mealsImage} alt="Hot Meals" />
+        </div>
+    </React.Fragment>
+};
+
+export default Header;
